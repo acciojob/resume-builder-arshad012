d3aa9a17efce9cdcf9ea69ac989bd8f2f6bc81ea
arshad012 <forarshad99@gmail.com>
2025-01-19 20:34:38 +0530

    Resume buildre App 2

--- src/components/PageChangingButton.jsx
+++ src/components/PageChangingButton.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const PageChangingButton = ({onclickFunc}) => {
     return (
         <div className="page-buttons">
-            <button className="link-btn">BACK</button>
-            <button className="normal-btn">NEXT</button>
+            <button id="previous" className="link-btn">BACK</button>
+            <button id="next" className="normal-btn">NEXT</button>
             <button className="normal-btn" onClick={onclickFunc}>SAVE AND CONTINUE</button>
         </div>
     )
